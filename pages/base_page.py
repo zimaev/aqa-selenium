affea7a3290d882d4d54016accd534e306527cfb
Anton Zimaev <zimaew@gmail.com>
2022-10-10 21:42:49 +0300

    lesson 4

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,6 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class BasePage:
@@ -7,3 +10,57 @@ class BasePage:
 
     def open(self):
         self.driver.get(self.url)
+
+    def element_is_visible(self, locator, timeout=30):
+        try:
+            return wait(self.driver, timeout).until(
+                EC.visibility_of_element_located(locator)
+            )
+        except Exception as e:
+            print(e)
+
+    def elements_are_visible(self, locator, timeout=30):
+        try:
+            return wait(self.driver, timeout).until(
+                EC.visibility_of_all_elements_located(locator)
+            )
+        except Exception as e:
+            print(e)
+
+    def element_is_presence(self, locator, timeout=30):
+        try:
+            return wait(self.driver, timeout).until(
+                EC.presence_of_element_located(locator)
+            )
+        except Exception as e:
+            print(e)
+
+    def elements_are_presence(self, locator, timeout=30):
+        try:
+            return wait(self.driver, timeout).until(
+                EC.presence_of_all_elements_located(locator)
+            )
+        except Exception as e:
+            print(e)
+
+    def element_is_not_visible(self, locator, timeout=30):
+        try:
+            return wait(self.driver, timeout).until(
+                EC.invisibility_of_element_located( locator)
+            )
+        except Exception as e:
+            print(e)
+
+    def element_is_clicable(self, locator, timeout=30):
+        try:
+            return wait(self.driver, timeout).until(
+                EC.element_to_be_clickable(locator)
+            )
+        except Exception as e:
+            print(e)
+
+    def scroll_to_element(self, element):
+        self.driver.execute_script("arguments[0].scrollIntoView();", element)
+
+
+
